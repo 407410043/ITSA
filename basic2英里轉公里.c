05a4053dee2f19cfa359b71c27cc0053d046b2f5
407410043 <59846616+407410043@users.noreply.github.com>
2020-01-14 14:17:41 +0800

    Add files via upload

---
+++ basic2英里轉公里.c
@@ -0,0 +1,12 @@
+#include<stdio.h>    
+int main(){     
+		int n;   
+		double i=0;   
+		while(scanf("%d",&n)!=EOF){  
+				i=0;  
+				i=n*1.6;  
+				printf("%.1f\n",i);  
+		}   
+		return 0;    
+}  
+
